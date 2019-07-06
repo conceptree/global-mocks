2cd9faa1cf11ea1ae2300fa4b972daa31b8a8ee7
Nuno Rodrigues <nunorodrigues@conceptree.com>
2019-07-07 00:24:13 +0100

    up and running

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# MY MOCK SERVER
+# GLOBAL MOCKS
 
-This is based on a mock-server package abstraction that ables you to create you global mock server for your different applications.
+If you are looking for a simple server that can provide your application requests with some mocked data without any fancy tricks, this project will help you out. 
+
+A simple implementation based on an express application server that, thought a simple configuration file, delivers your mocks to any request that your apps perform to specific endpoints. This will keep your development workflow nice and smooth guarantying that you cover all application features running with the planned data structures until their are ready for production.
 
 ## Getting Started
 
 1) Clone this project to a folder of your choice from where you want to serve your mocked data.
 2) Run npm install
-3) Create the folders for each of your mocked data. This folder structure should respect the following hierarchy:
-   * your-service-name/api-path/specific-api-paths/even-more-specific-api-paths/
+3) Create your mocked data JSON based files and save them into the project data folder.
+4) Set into the config.json file all your services. 
 
-    * For example, if your request is pointing to http://hostname:port/api/users you should create a folder for that particular service requests like user-data and then create a folder inside it called api and inside the api folder another one called users.
-  4) Inside each folder you must create your mocks data files like:
-        * your-service/api/users/GET.mock
-        * GET, POST, UPDATE, DELETE whatever you need to respond to.
-
-### GET.mock
-    HTTP/1.1 200 OK
-    Content-Type: application/json; charset=utf-8
-
+### config.json
     {
-    "my-key": "my-value"
+      "services": [
+          {
+              "name":"global-service",
+              "port":9001,
+              "host":"127.0.0.1",
+              "methods":[
+                  {
+                      "type": "get",
+                      "path": "/api/users/",
+                      "mocks": "users.json"
+                  }
+              ]
+          }
+      ]
     }
 
+### Have fun! 
--- server.js
+++ server.js
@@ -1,46 +1,58 @@
 var config = require('./config.json');
 var express = require('express');
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
 var app = express();
+var appRoot = __dirname.replace(/\\/g, '/');
 var userInput = process.stdin;
-userInput.setEncoding('utf-8');
 var services = config.services;
 var menuLength = 0;
 
-if(services.length > 0){
-    console.log("What service should I start?");
-    services.forEach(function(element, index){
-        console.log(index+1+") "+element.name);
-        menuLength = index+1;
-    });
-}else{
-    console.log("No services on your config file.");
-    process.exit();
+userInput.setEncoding('utf-8');
+app.use(express.json());
+
+if (services.length > 0) {
+	console.log('What service should I start?');
+	services.forEach((element, index) => {
+		console.log(index + 1 + ') ' + element.name);
+		menuLength = index + 1;
+	});
+} else {
+	console.log('No services on your config file.');
+	process.exit();
 }
 
-userInput.on('data', function(data){
-    data = Number(data);
-    if(data){
-        if(data > menuLength || data < 0){
-            console.log("Your choice isn't in the list!");
-        }
-        startTheServer(data-1);
-    }else{
-        console.log("Please input a number that matches your menu selection.");
-    }
-
+userInput.on('data', (data) => {
+	data = Number(data);
+	if (data) {
+		if (data > menuLength || data < 0) {
+			console.log("Your choice isn't in the list!");
+		}
+		startTheServer(data - 1);
+	} else {
+		console.log('Please input a number that matches your menu selection.');
+	}
 });
 
-function startTheServer(serviceIndex){
-    var httpServer = http.createServer(app);
-    var host = services[serviceIndex].host;
-    var port = services[serviceIndex].port;
-    httpServer.listen(port, host, function(){
-        console.log("Up and running on http://"+host+":"+port);
-    });
+function startTheServer(serviceIndex) {
+	var httpServer = http.createServer(app);
+	var host = services[serviceIndex].host;
+	var port = services[serviceIndex].port;
+	httpServer.listen(port, host, function() {
+		console.log('Up and running on http://' + host + ':' + port);
+	});
+	startMethods(services[serviceIndex].methods);
 }
 
-app.get('/', function(req, res){
-    res.header('Content-type', 'application/json');
-    return res.end('<h1>Hello, Secure World!</h1>');
-});
+function startMethods(methods) {
+	methods.forEach(function(element) {
+        var mocksPath = path.join(appRoot + '/data/', element.mocks);
+        var data = fs.readFileSync(mocksPath, 'utf8');
+        app[element.type](element.path, (req, res)=>{
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200);
+            res.send(data);
+        });
+	});
+}
