1d7957d3744688b643c32193c645319af26101b8
Nuno Rodrigues <nunomtrodrigues@gmail.com>
2019-07-08 23:41:57 +0100

    https configuration added

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # GLOBAL MOCKS
 
-If you are looking for a simple server that can provide your application requests with some mocked data without any fancy tricks, this project will help you out. 
+If you are looking for a simple server that can provide your application requests with some mocked data without any fancy tricks, this project will help you out.
 
-A simple implementation based on an express application server that, thought a simple configuration file, delivers your mocks to any request that your apps perform to specific endpoints. This will keep your development workflow nice and smooth guarantying that you cover all application features running with the planned data structures until their are ready for production.
+A simple implementation based on an express application server that, thought a simple configuration file, delivers your mocks to any request that your apps perform to specific endpoints. This will keep your development workflow nice and smooth guarantying that you cover all application features running with the planned data structures until they are ready for production.
 
 ## Getting Started
 
 1) Clone this project to a folder of your choice from where you want to serve your mocked data.
 2) Run npm install
 3) Create your mocked data JSON based files and save them into the project data folder.
-4) Set into the config.json file all your services. 
+4) Set into the config.json file all your services.
 
 ### config.json
     {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var config = require('./config.json');
 var express = require('express');
 var http = require('http');
+var https = require('https');
 var path = require('path');
 var fs = require('fs');
 var app = express();
@@ -18,41 +19,58 @@ if (services.length > 0) {
 		console.log(index + 1 + ') ' + element.name);
 		menuLength = index + 1;
 	});
+	console.log(menuLength + 1 + ') Exit');
 } else {
-	console.log('No services on your config file.');
+	console.log('Please set your services on your config file.');
 	process.exit();
 }
 
 userInput.on('data', (data) => {
 	data = Number(data);
 	if (data) {
-		if (data > menuLength || data < 0) {
+		if (data > menuLength + 1 || data < 0) {
 			console.log("Your choice isn't in the list!");
+		} else if (data === menuLength + 1) {
+			process.exit();
+		} else {
+			startTheServer(data - 1);
 		}
-		startTheServer(data - 1);
 	} else {
 		console.log('Please input a number that matches your menu selection.');
 	}
 });
 
 function startTheServer(serviceIndex) {
-	var httpServer = http.createServer(app);
+	if (services[serviceIndex].type === "http") {
+		try{
+			var httpServer = http.createServer(app);
+		}catch(e){
+			console.log('Unable to create server! Please try again!');
+		}
+	} else {
+		try {
+			var httpServer = https.createServer(services[serviceIndex].options, app);
+		} catch (e) {
+			console.log('Unable to create server, make sure you have the right credentials.');
+			process.exit();
+		}
+	}
 	var host = services[serviceIndex].host;
 	var port = services[serviceIndex].port;
-	httpServer.listen(port, host, function() {
-		console.log('Up and running on http://' + host + ':' + port);
+	httpServer.listen(port, host, function () {
+		console.log('Running at ' + httpServer.address().address + ':' + httpServer.address().port);
 	});
 	startMethods(services[serviceIndex].methods);
 }
 
 function startMethods(methods) {
-	methods.forEach(function(element) {
-        var mocksPath = path.join(appRoot + '/data/', element.mocks);
-        var data = fs.readFileSync(mocksPath, 'utf8');
-        app[element.type](element.path, (req, res)=>{
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200);
-            res.send(data);
-        });
+	methods.forEach(function (element) {
+		var mocksPath = path.join(appRoot + '/data/', element.mocks);
+		var data = fs.readFileSync(mocksPath, 'utf8');
+		app[element.type](element.path, (req, res) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.status(200);
+			res.send(data);
+		});
 	});
 }
